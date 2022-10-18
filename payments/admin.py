ddac5b3ba96832dc3cafaec6a3e44acdecf91003
Joshua <cutejosh2@gmail.com>
2022-10-18 14:05:51 +0100

    initiate payment view

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Payment
 
-# Register your models here.
+class PaymentAdmin(admin.ModelAdmin):
+	list_display = ["id", "ref", 'amount', "verified", "date_created"]
+
+
+admin.site.register(Payment, PaymentAdmin)
--- payments/views.py
+++ payments/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Payment
+from django.conf import settings
 
-# Create your views here.
+def initiate_payment(request):
+	if request.method == "POST":
+		amount = request.POST['amount']
+		email = request.POST['email']
+
+		pk = settings.PAYSTACK_PUBLIC_KEY
+
+		payment = Payment.objects.create(amount=amount, email=email, user=request.user)
+		payment.save()
+
+		context = {
+			'payment': payment,
+			'field_values': request.POST,
+			'paystack_pub_key': pk,
+			'amount_value': payment.amount_value(),
+		}
+		return render(request, 'make_payment.html', context)
+
+	return render(request, 'payment.html')
