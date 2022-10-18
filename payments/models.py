b764af3362d9d661d49683107abd0279becb94ed
Joshua <cutejosh2@gmail.com>
2022-10-18 11:43:40 +0100

    installed apps and basic models setup

--- payments/models.py
+++ payments/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+import secrets
 
 # Create your models here.
+class UserWallet(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    currency = models.CharField(max_length=50, default='NGN')
+    created_at = models.DateTimeField(default=timezone.now, null=True)
+
+    def __str__(self):
+        return self.user.__str__()
+
+class Payment(models.Model):
+	user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+	amount = models.PositiveIntegerField()
+	ref = models.CharField(max_length=200)
+	email = models.EmailField()
+	verified = models.BooleanField(default=False)
+	date_created = models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		ordering = ('-date_created',)
+
+	def __str__(self):
+		return f"Payment: {self.amount}"
+
+	def save(self, *args, **kwargs):
+		while not self.ref:
+			ref = secrets.token_urlsafe(50)
+			object_with_similar_ref = Payment.objects.filter(ref=ref)
+			if not object_with_similar_ref:
+				self.ref = ref
+
+		super().save(*args, **kwargs)
--- zcore/settings.py
+++ zcore/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "accounts",
-    "zcore",
+    'accounts',
+    'payments',
 ]
 
 MIDDLEWARE = [
